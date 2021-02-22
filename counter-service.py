f81b5ad746145fd488c7b98e8931fff0e43b99f2
achisolomon <35426030+achisolomon@users.noreply.github.com>
2021-02-22 18:54:54 +0200

    Update counter-service.py

--- counter-service.py
+++ counter-service.py
@@ -3,15 +3,13 @@ from flask import Flask, request, request_started
 
 app = Flask(__name__)
 counter = 0
-@app.before_request
-def before_request():
-    """Hmmm, Plus 1 please"""
-    global counter
-    if request.method=='POST':
-        counter+=1
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def index():
-     """Our counter is..."""   
-     return str(counter)
+    global counter
+    if request.method == "POST":
+        counter+=1
+        return "Hmm, Plus 1 please "
+    else:
+        return str(f"Our counter is: {counter} ")
 if __name__ == '__main__':
-    app.run(debug=True,port=80)
+    app.run(debug=True,port=80,host='0.0.0.0')
